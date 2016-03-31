9916c56c98c8535526b3e926c45f6ca19d9fbfb1
Randy Gylling <MikeGylling@yahoo.com>
2016-03-30 19:54:55 -0700

    finished up some minor tweaks

--- buss.js
+++ buss.js
@@ -27,23 +27,6 @@ var processClick = true;
 var processClickOne = true;
 
 
-/*
-variables capturing the paragraph slots
-for textContent addition on show results
-*/
-
-/*
-variables for paragraph elements to be added per
-image and add paragraph elements within html
-*/
-var paraZero = document.getElementById('paraZero');
-var paraOne = document.getElementById('paraOne');
-var paraTwo = document.getElementById('paraTwo');
-var paraThree = document.getElementById('paraThree');
-var paraFour = document.getElementById('paraFour');
-var paraFive = document.getElementById('paraFive');
-var paraSix = document.getElementById('paraSix');
-var paraSeven = document.getElementById('paraSeven');
 
 /*
 ===============
@@ -157,8 +140,6 @@ processClick = false;
 }
 
 
-
-
 //constructor function to make new image objects
 function makeImageObj(name, path) {
   this.name = name;
@@ -189,24 +170,9 @@ function showRandomImg(image) {
   myArray[n].nShow++;
 }
 
-
-
 //function to show results
 function showResults() {
-  /*
-  console.log(totalClicks + " this is working yes?");
-  paraZero.textContent = "the total number of clicks : " + totalClicks;
-  // ++++++++++
-  //add in paraNum.textContent per paragraph element position within HTML DOM
 
-  paraOne.textContent = "the number of times the beanie was clicked on: " + myArray[0].nClicks + " times and the image was shown : " + myArray[0].nShow + " times.";
-  paraTwo.textContent = "the number of times the beer bong was clicked on: " + myArray[1].nClicks + " times and the image was shown : " + myArray[1].nShow + " times.";
-  paraThree.textContent = "the number of times the boot cup was clicked on: " + myArray[2].nClicks + " times and the image was shown : " + myArray[2].nShow + " times.";
-paraFour.textContent = "the number of times comb was clicked on: " + myArray[3].nClicks + " times and the image was shown : " + myArray[3].nShow + " times.";
-paraFive.textContent = "the number of times keg was clicked on: " + myArray[4].nClicks + " times and the image was shown : " + myArray[4].nShow + " times.";
-paraSix.textContent = "the number of times oil was clicked on: " + myArray[5].nClicks + " times and the image was shown : " + myArray[5].nShow + " times.";
-paraSeven.textContent = "the number of times vans was clicked on: " + myArray[6].nClicks + " times and the image was shown : " + myArray[6].nShow + " times." + myArray[6].percentage();
-*/
 reset.setAttribute('style','visibility:visible');
 displayButton.setAttribute('style','visibility:hidden');
 voteMoreButton.setAttribute('style','visibility:hidden');
@@ -228,12 +194,7 @@ var buyerData = {
 };
 
 new Chart(buyers).Bar(buyerData);
-
-
-
-
 var buyersOne = document.getElementById('buyersOne').getContext('2d');
-
 
 var buyerDataOne = {
 	labels : ["Beanie","Beer Bong","Boot Glass","Comb","Keg","Oil","Vans"],
@@ -254,7 +215,7 @@ new Chart(buyersOne).Bar(buyerDataOne);
 
 }
 
-var imageOneCounter = 0;
+
 
 /*
 ===============
@@ -276,24 +237,3 @@ function resetThePage () {
   location.reload();
   window.scrollTo(0, 0);
 }
-
-
-/*
-var buyers = document.getElementById('buyers').getContext('2d');
-
-
-var buyerData = {
-	labels : ["Vans","Comb","Oil","Beer Bong","Boot","Keg","Vans"],
-	datasets : [
-		{
-			fillColor : "rgba(183,211,231,0.4)",
-			strokeColor : "#617E94",
-			pointColor : "#fff",
-			pointStrokeColor : "#9DB86D",
-			data : [myArray[0].nClicks,myArray[0].nClicks,myArray[0].nClicks]
-		}
-	]
-};
-
-new Chart(buyers).Bar(buyerData);
-*/
